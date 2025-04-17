e81cf526e9a007060ae92e167de6bc4650af8b39
DuckTheDukc <166407070+DuckTheDukc@users.noreply.github.com>
2025-04-17 16:42:51 +0400

    delete manekens

--- Assets/scripts/GunRaycast.cs
+++ Assets/scripts/GunRaycast.cs
@@ -9,7 +9,7 @@ public class GunRaycast : MonoBehaviour
     public float range = 100f;          // Дальность стрельбы
     public float fireRate = 15f;        // Скорострельность (выстрелов в секунду)
     public Camera fpsCam;               // Камера игрока (откуда летит луч)
-    public ParticleSystem muzzleFlash;  // Эффект дульного огня
+    public GameObject gunShotEffectPrefab;  // Префаб с партиклами
     public GameObject impactEffect;     // Эффект попадания (например, искры)
 
     private float nextTimeToFire = 0f;   // Таймер для скорострельности
@@ -27,8 +27,12 @@ public class GunRaycast : MonoBehaviour
     void Shoot()
     {
         // 1. Проигрываем дульный огонь
-        muzzleFlash.Play();
-
+        GameObject effect = Instantiate(gunShotEffectPrefab, transform.position, transform.rotation);
+        Destroy(effect, 2f);
+        foreach (ParticleSystem ps in effect.GetComponentsInChildren<ParticleSystem>())
+        {
+            ps.Play();
+        }
         // 2. Пускаем луч из центра камеры
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
@@ -37,6 +41,8 @@ public class GunRaycast : MonoBehaviour
             // 4. Создаём эффект попадания
             GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
             Destroy(impact, 2f); // Удаляем через 2 секунды
+
+            // Проверяем, попали ли в часть манекена
         }
     }
 }
